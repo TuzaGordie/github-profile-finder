8ba2f749087e17f2f36ee1298f79012bcc9c12cf
XodTuza <gordie2u@gmail.com>
2020-06-16 01:07:50 +0100

    created services and connected to github api.

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../shared/services/profile.service';
+import {HttpResponse} from '@angular/common/http';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  profile: HttpResponse<any>;
+  repos: HttpResponse<any>;
+
+  constructor(private profileService: ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this.profileService.getProfileRepo().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/services/profile.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+
+  username: string;
+  clientId: '5a0ad62deb6ad25afd58';
+  clientSecret: '998ff3911fd8eafe3d344e7d034c4b10a1e86c57';
+  constructor(private http: HttpClient) {
+    console.log('service is ready!!!');
+    this.username = 'TuzaGordie';
+  }
+
+  getProfileInfo(): Observable<HttpResponse<any>> {
+    return this.http
+      .get<HttpResponse<any>>('https://api.github.com/users/' + this.username + '?client_id=' + this.clientId + '&client_secret' +
+        this.clientSecret, {observe: 'response'}).pipe(map((res) => res.body));
+  }
+
+  getProfileRepo(): Observable<HttpResponse<any>> {
+    return this.http
+      .get<HttpResponse<any>>('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientId + '&client_secret' +
+        this.clientSecret, {observe: 'response'}).pipe(map((res) => res.body));
+  }
+}
